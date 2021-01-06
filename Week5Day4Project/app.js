e25de41c39670ae8922e722d19734b23c81a8e79
sherylmoser <sherylrmoser@gmail.com>
2021-01-05 20:16:17 -0700

    Week 5 Day 4 project added

---
+++ Week5Day4Project/app.js
@@ -0,0 +1,5 @@
+$('.navItem').css('color', 'white');
+$('#nav').css('background-color', '#145');
+$('li:even').css('background-color', '#ccc');
+$('li:odd').css('background-color', '#aaa');
+$('input').val("Sheryl");
